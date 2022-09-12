7b3fc682e3240290a3d7621afb7fe29f74d1e25b
dmitri <dmitriklsv@gmail.com>
2022-09-12 16:26:30 +0600

    Time: 9 ms (8.66%), Space: 2.6 MB (70.00%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.go
@@ -0,0 +1,28 @@
+func search(nums []int, target int) int {
+    left:=0
+    right:=len(nums)-1
+    for left<=right {
+        mid:=left+(right-left)/2
+        switch {
+            case nums[mid]>target:
+                if target<nums[left] && nums[mid]>nums[right] {
+                    left=mid+1
+                } else {
+                    right=mid-1
+                }
+            case nums[mid]<target: 
+                if nums[right]<target && nums[mid]<nums[left] {
+                    right=mid-1
+                } else {
+                    left=mid+1
+                }
+            default:
+            return mid
+        }
+    }
+    
+    return -1
+}
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4

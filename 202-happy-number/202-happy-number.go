5d5cdd3946941eed855757c2258c0ad5469d2673
dmitri <dmitriklsv@gmail.com>
2022-09-27 16:10:17 +0600

    Time: 3 ms (44.82%), Space: 2.1 MB (64.50%) - LeetHub

--- 202-happy-number/202-happy-number.go
+++ 202-happy-number/202-happy-number.go
@@ -1,10 +1,10 @@
 func isHappy(n int) bool {
-    table:=make(map[int]bool)
+    table:=make(map[int]struct{})
     for n != 1 {
         if _,ok := table[n]; ok {
             return false
         }
-        table[n]=true
+        table[n]=struct{}{}
         num:=0
         for n>0 {
             num+=(n%10)*(n%10)

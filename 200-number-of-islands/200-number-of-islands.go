1f5c74de8863bbd6f3be70bf01124140107b954d
dmitri <dmitriklsv@gmail.com>
2022-12-01 19:19:23 +0500

    Time: 26 ms (18.93%), Space: 6.9 MB (18.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.go
+++ 200-number-of-islands/200-number-of-islands.go
@@ -1,39 +1,42 @@
 func numIslands(grid [][]byte) int {
-    count := 0
-    
-    for i := 0; i < len(grid); i++ {
-        for j := 0; j < len(grid[i]); j++ {
+    var c int
+    var visit [][]int
+   
+    for i:=0;i<len(grid);i++ {
+        for j:=0;j<len(grid[i]);j++ {
             if grid[i][j] == '1' {
-                count++
-                
-                // dfs(grid, i, j)
-				bfs(grid, i, j)
+                visit = append(visit,[]int{i,j})
+                bfs(&grid,i,j,&visit)
+                c++
             }
         }
     }
     
-    return count
+    return c
 }
 
-func bfs(grid [][]byte, r, c int) {
-    q := [][2]int{}
+func bfs(grid *[][]byte,i,j int,visit *[][]int) {
+    if j<len((*grid)[i])-1 && (*grid)[i][j+1]=='1'{
+        *visit=append(*visit,[]int{i,j+1})
+        (*grid)[i][j+1]='x'
+    }
+    if i<len(*grid)-1 && (*grid)[i+1][j]=='1'{
+        *visit=append(*visit,[]int{i+1,j})
+        (*grid)[i+1][j]='x'
+    }
+    if j>0 && (*grid)[i][j-1]=='1' {
+        *visit=append(*visit,[]int{i,j-1})
+        (*grid)[i][j-1]='x'
+    }
+    if i>0 && (*grid)[i-1][j]=='1' {
+        *visit=append(*visit,[]int{i-1,j})
+        (*grid)[i-1][j]='x'
+    }
     
-    q = append(q, [2]int{r,c})
-    grid[r][c] = '2'
+    *visit=(*visit)[1:]
+    (*grid)[i][j]='x'
     
-    offsets := [4][2]int{{-1,0}, {1,0}, {0,1}, {0,-1}}
-    for len(q) > 0 {
-        curr := q[0]
-        q = q[1:]
-        
-        for _, offset := range offsets {
-            x := curr[0] + offset[0]
-            y := curr[1] + offset[1]
-            
-            if x >= 0 && x < len(grid) && y >= 0 && y < len(grid[x]) && grid[x][y] == '1' {
-                q = append(q, [2]int{x,y})
-                grid[x][y] = 2
-            }
-        }
+    if len(*visit) != 0 {
+        bfs(grid,(*visit)[0][0],(*visit)[0][1],visit)
     }
 }

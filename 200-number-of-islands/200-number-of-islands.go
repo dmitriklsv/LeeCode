0b09739a30718d2ec3f385c0700037e8634ab04e
dmitri <dmitriklsv@gmail.com>
2022-12-01 19:17:10 +0500

    Time: 4 ms (85.10%), Space: 5.5 MB (35.95%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.go
+++ 200-number-of-islands/200-number-of-islands.go
@@ -1,26 +1,39 @@
 func numIslands(grid [][]byte) int {
-    var c int
-    for i:=0;i<len(grid);i++{
-        for j:=0;j<len(grid[i]);j++ {
+    count := 0
+    
+    for i := 0; i < len(grid); i++ {
+        for j := 0; j < len(grid[i]); j++ {
             if grid[i][j] == '1' {
-                dfs(&grid,i,j)
-                c++
+                count++
+                
+                // dfs(grid, i, j)
+				bfs(grid, i, j)
             }
         }
     }
     
-    return c
+    return count
 }
 
-func dfs(grid *[][]byte,i,j int) {
-    if  i < 0 || i > len(*grid)-1 || j > len((*grid)[i])-1 || j < 0 || (*grid)[i][j] != '1' {
-        return
+func bfs(grid [][]byte, r, c int) {
+    q := [][2]int{}
+    
+    q = append(q, [2]int{r,c})
+    grid[r][c] = '2'
+    
+    offsets := [4][2]int{{-1,0}, {1,0}, {0,1}, {0,-1}}
+    for len(q) > 0 {
+        curr := q[0]
+        q = q[1:]
+        
+        for _, offset := range offsets {
+            x := curr[0] + offset[0]
+            y := curr[1] + offset[1]
+            
+            if x >= 0 && x < len(grid) && y >= 0 && y < len(grid[x]) && grid[x][y] == '1' {
+                q = append(q, [2]int{x,y})
+                grid[x][y] = 2
+            }
+        }
     }
-    
-    (*grid)[i][j] = 'x'
-    
-    dfs(grid,i,j+1)
-    dfs(grid,i+1,j)
-    dfs(grid,i,j-1)
-    dfs(grid,i-1,j)
 }

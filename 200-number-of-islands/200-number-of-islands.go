839a8305dcaaa84e2a3472e4ee67b171e65a8104
dmitri <dmitriklsv@gmail.com>
2022-12-01 16:58:53 +0500

    Time: 3 ms (94.16%), Space: 3.8 MB (99.70%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.go
+++ 200-number-of-islands/200-number-of-islands.go
@@ -3,7 +3,7 @@ func numIslands(grid [][]byte) int {
     for i:=0;i<len(grid);i++{
         for j:=0;j<len(grid[i]);j++ {
             if grid[i][j] == '1' {
-                bfs(&grid,i,j)
+                dfs(&grid,i,j)
                 c++
             }
         }
@@ -12,15 +12,15 @@ func numIslands(grid [][]byte) int {
     return c
 }
 
-func bfs(grid *[][]byte,i,j int) {
+func dfs(grid *[][]byte,i,j int) {
     if  i < 0 || i > len(*grid)-1 || j > len((*grid)[i])-1 || j < 0 || (*grid)[i][j] != '1' {
         return
     }
     
     (*grid)[i][j] = 'x'
     
-    bfs(grid,i,j+1)
-    bfs(grid,i+1,j)
-    bfs(grid,i,j-1)
-    bfs(grid,i-1,j)
+    dfs(grid,i,j+1)
+    dfs(grid,i+1,j)
+    dfs(grid,i,j-1)
+    dfs(grid,i-1,j)
 }
